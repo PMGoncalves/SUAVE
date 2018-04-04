54c3de036d0c344063f8c203d6152d6137557eef
P. Goncalves <pedro.mg.94@hotmail.com>
2018-04-04 02:36:07 +0100

    hmm

--- trunk/SUAVE/Components/Energy/Processes/Thrust.py
+++ trunk/SUAVE/Components/Energy/Processes/Thrust.py
@@ -93,6 +93,13 @@ class Thrust(Energy_Component):
         return Cpw
 
 
+    def theta_beta_mach(M0, theta, gamma, delta):
+        l1 = np.sqrt(((M0**2-1)**2)-3*((1+((gamma-1)/2)*M0**2)*(1+((gamma+1)/2)*M0**2))*(np.tan(theta))**2)
+        l2 = ((M0**2-1)**3-9*(1+(gamma-1)/2*M0**2)*(1+(gamma-1)/2*M0**2+(gamma+1)/4*M0**4)*(np.tan(theta))**2)/(l1**3)
+        beta = np.arctan((M0**2-1+2*l1*np.cos((4*np.pi*delta+np.arccos(l2))/3))/(3*(1+(gamma-1)/2*M0**2)*np.tan(theta)))
+    
+        return beta    
+
     
     def thermo_anderson(self,conditions):
         
@@ -125,115 +132,114 @@ class Thrust(Energy_Component):
         
 #        #---------------------------------------------------------
 #                
-#        return
-#        from SUAVE.Methods.Propulsion.oblique_shock import theta_beta_mach
-#        from SUAVE.Attributes.Gases import Air
-#        # unpacking from conditions
-#        gamma                = conditions.freestream.isentropic_expansion_factor
-#        Cp                   = conditions.freestream.specific_heat_at_constant_pressure
-#        R                    = conditions.freestream.specific_gas_constant
-#        Vo                   = conditions.freestream.velocity
-#        To                   = conditions.freestream.temperature
-#        a0                   = conditions.freestream.speed_of_sound
-#        M                    = conditions.freestream.mach_number
-#        Po                   = conditions.freestream.pressure  
-#        g                    = conditions.freestream.gravity
-#        a                    = conditions.freestream.speed_of_sound
-#        rho                  = conditions.freestream.density
-#        mew                  = conditions.freestream.dynamic_viscosity
-#        Tto                   = conditions.freestream.stagnation_temperature
-#        Pto                  = conditions.freestream.stagnation_pressure
-#        gas = Air()
-##
-#        Pr = 0.715
-#        Tto = conditions.freestream.stagnation_temperature   
-#        Pto = conditions.freestream.stagnation_pressure
+        from SUAVE.Methods.Propulsion.oblique_shock import theta_beta_mach
+        from SUAVE.Attributes.Gases import Air
+        # unpacking from conditions
+        gamma                = conditions.freestream.isentropic_expansion_factor
+        Cp                   = conditions.freestream.specific_heat_at_constant_pressure
+        R                    = conditions.freestream.gas_specific_constant
+        Vo                   = conditions.freestream.velocity
+        To                   = conditions.freestream.temperature
+        a0                   = conditions.freestream.speed_of_sound
+        M                    = conditions.freestream.mach_number
+        Po                   = conditions.freestream.pressure  
+        g                    = conditions.freestream.gravity
+        a                    = conditions.freestream.speed_of_sound
+        rho                  = conditions.freestream.density
+        mew                  = conditions.freestream.dynamic_viscosity
+        Tto                   = conditions.freestream.stagnation_temperature
+        Pto                  = conditions.freestream.stagnation_pressure
+        gas = Air()
+
+        Pr = 0.715
+        Tto = conditions.freestream.stagnation_temperature   
+        Pto = conditions.freestream.stagnation_pressure
 #        
-#        #---- Geometry
-#        Rle     = 3.0e-3                  # Leading edge radius
-#        L_flat  = 82.3e-3                # Lenght flat plate   
-#        theta_f = 84.*np.pi/180.         # radians
-#        phi     = np.pi/2. - theta_f    # radians
-#        
-#        #---- Material
-#        eps     = 0.95      # emissivity
-#        
-#        #---- Constants
-#        sig     = 5.67e-8   # Steffan-Boltzman constant
-##    
-##    
-#        #-- Stagnation
-#        rhoo    = Pto/(R*Tto)
+        #---- Geometry
+        Rle     = 3.0e-3                  # Leading edge radius
+        L_flat  = 82.3e-3                # Lenght flat plate   
+        theta_f = 84.*np.pi/180.         # radians
+        phi     = np.pi/2. - theta_f    # radians
+        
+        #---- Material
+        eps     = 0.95      # emissivity
+        
+        #---- Constants
+        sig     = 5.67e-8   # Steffan-Boltzman constant
+#    
+#    
+        #-- Stagnation
+        rhoo    = Pto/(R*Tto)
 ##        
-#        #-- Shock
-#        Tt1     = Tto
-#        Pt1     = Pto * ((((gamma+1.)*M*M)/(2.+(gamma-1.)*M*M))**(gamma/(gamma-1.)))*((gamma+1.)/(2.*gamma*M*M-(gamma-1.)))**(1./(gamma-1.))
-#        rho1    = Pt1/(R*Tt1)
-#        mew1    = gas.compute_absolute_viscosity(Tt1)
+        #-- Shock
+        Tt1     = Tto
+        Pt1     = Pto * ((((gamma+1.)*M*M)/(2.+(gamma-1.)*M*M))**(gamma/(gamma-1.)))*((gamma+1.)/(2.*gamma*M*M-(gamma-1.)))**(1./(gamma-1.))
+        rho1    = Pt1/(R*Tt1)
+        mew1    = gas.compute_absolute_viscosity(Tt1)
+        
+        #-- Walls
+        haw     = Cp*To + Vo*Vo/2.       #adiabatic wall enthalpy
+        duds    = (1./Rle)*np.sqrt(2.*(Pt1-Po)/rho1)  
+        #------------------------
+    
+        def integrand(s, Tiso):
+            
+            #-- Wall Cp    
+            Cpw1 = Cpw(Tiso)
+            
+                
+            #-- Position
+            theta = s/Rle
+            #-- Stagnation point heat flux
+            q_st_gw = 0.57 * Pr**(-0.6) * (rho1*mew1)**0.5 * (haw - Cpw1*Tiso) * duds**0.5
+            
+            #-- Around the tip curvature heat flux
+            G           = (1. - 1./(gamma*M*M))*(theta*theta - theta/2.*np.sin(4.*theta) + (1.-np.cos(4.*theta))/8.) + 4./(gamma*M*M)*((theta*theta - theta*np.sin(2.*theta) + (1.-np.cos(2.*theta))/2.))
+            
+            qt_qst  = 2.*theta*np.sin(theta)*((1.-(1./(gamma*M*M)))*(np.cos(theta))**2. + 1./(gamma*M*M))*G**-0.5
+    
+            qr      = sig*eps*Tiso**4.
+            
+            
+            return q_st_gw*qt_qst - qr
+#    
 #        
-#        #-- Walls
-#        haw     = Cp*To + Vo*Vo/2.       #adiabatic wall enthalpy
-#        duds    = (1/Rle)*np.sqrt(2*(Pt1-Po)/rho1)  
-#        #------------------------
-#    
-#        def integrand(s, Tiso):
 #            
-#            #-- Wall Cp    
-#            Cpw1 = Cpw(Tiso)
-#            
+        def integrand2(y,Tiso) :
+            #-- Flat plate
+            
+            #-- Wall Cp    
+            Cpw1 = Thrust.Cpw(Tiso)
+            
+            #---- After shock conditions
+            beta    = theta_beta_mach(M, phi, gamma,1 )
+            Msin    = M*np.sin(beta)
+            M2      = ((1/(np.sin(beta-phi))**2.)*((Msin)**2.+(2./(gamma-1.)))/((2.*gamma*Msin**2.)/(gamma-1.)-1.))**0.5
+            P2      = p*(1.+((2.*gamma)/(1.+gamma))*(Msin**2.-1.))
+            rho2    = rhoo*(gamma+1.)*Msin*Msin/(2.+(gamma-1.)*Msin*Msin)
+            V2      = Vo*(M2/M)*((P2/p)*(rhoo/rho2))**(0.5)
+            T2      = ((P2*rhoo)/(p*rho2))*T          
+            mew2    = gas.compute_absolute_viscosity(T2)
 #                
-#            #-- Position
-#            theta = s/Rle
-#            #-- Stagnation point heat flux
-#            q_st_gw = 0.57 * Pr**(-0.6) * (rho1*mew1)**0.5 * (haw - Cpw1*Tiso) * duds**0.5
-#            
-#            #-- Around the tip curvature heat flux
-#            G           = (1. - 1./(gamma*M*M))*(theta*theta - theta/2.*np.sin(4.*theta) + (1.-np.cos(4.*theta))/8.) + 4./(gamma*M*M)*((theta*theta - theta*np.sin(2.*theta) + (1.-np.cos(2.*theta))/2.))
-#            
-#            qt_qst  = 2.*theta*np.sin(theta)*((1.-(1./(gamma*M*M)))*(np.cos(theta))**2. + 1./(gamma*M*M))*G**-0.5
-#    
-#            qr      = sig*eps*Tiso**4.
-#            
-#            
-#            return q_st_gw*qt_qst - qr
-#    
-#        
-#            
-#        def integrand2(y,Tiso) :
-#            #-- Flat plate
-#            
-#            #-- Wall Cp    
-#            Cpw1 = Thrust.Cpw(Tiso)
-#            
-#            #---- After shock conditions
-#            beta    = theta_beta_mach(M, phi, gamma,1 )
-#            Msin    = M*np.sin(beta)
-#            M2      = ((1/(np.sin(beta-phi))**2.)*((Msin)**2.+(2./(gamma-1.)))/((2.*gamma*Msin**2.)/(gamma-1.)-1.))**0.5
-#            P2      = p*(1.+((2.*gamma)/(1.+gamma))*(Msin**2.-1.))
-#            rho2    = rhoo*(gamma+1.)*Msin*Msin/(2.+(gamma-1.)*Msin*Msin)
-#            V2      = Vo*(M2/M)*((P2/p)*(rhoo/rho2))**(0.5)
-#            T2      = ((P2*rhoo)/(p*rho2))*T          
-#            mew2    = gas.compute_absolute_viscosity(T2)
-#                
-#            #---- Wall enthalpy
-#            h2aw    = Cp*T2 + r*V2*V2/2.
-#            
-#            #---- Find wall temperature
-#            
-#            Cpw2      = Cpw(3000)
-#            T2aw    = h2aw / Cpw2
-#                 
-#            Pr_star = (((T2aw/T2)-1.)*(2./((gamma-1.)*M2*M2)))**2.
-#            Re_y    = rho2*V2*y/mew2
-#            
-#            Tstar   = T2*(0.5 + 0.039*M2*M2 + 0.5*Tiso/T2)
-#            Cfstar  = 0.664*(Tstar/T2)**(-1./6.)*Re_y**-0.5
-#            CH      = Cfstar/(2.*Pr_star**(2./3.))
-#            q_flat  = CH*rho2*V2*(h2aw-Cpw1*Tiso)
-#            
-#            qr      = sig*eps*Tiso**4.
-#            
-#            return q_flat - qr
+            #---- Wall enthalpy
+            h2aw    = Cp*T2 + r*V2*V2/2.
+            
+            #---- Find wall temperature
+            
+            Cpw2      = Cpw(3000)
+            T2aw    = h2aw / Cpw2
+                 
+            Pr_star = (((T2aw/T2)-1.)*(2./((gamma-1.)*M2*M2)))**2.
+            Re_y    = rho2*V2*y/mew2
+            
+            Tstar   = T2*(0.5 + 0.039*M2*M2 + 0.5*Tiso/T2)
+            Cfstar  = 0.664*(Tstar/T2)**(-1./6.)*Re_y**-0.5
+            CH      = Cfstar/(2.*Pr_star**(2./3.))
+            q_flat  = CH*rho2*V2*(h2aw-Cpw1*Tiso)
+            
+            qr      = sig*eps*Tiso**4.
+            
+            return q_flat - qr
 #
 #
         
